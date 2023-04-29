c82954afa03777232aa872703d40fdc0c03797a5
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-04-29 13:52:16 -0500

    UI main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 export default function IndexPage() {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-      </p>
+      <Grid container>
+        <Grid
+          item
+          xs={12}>
+          <Typography variant="h1">NextAuth.js Example</Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          pt={5}
+          justifyContent="flex-start">
+          <Typography variant="body1">
+            This is an example site to demonstrate how to use{" "}
+            <a href="https://next-auth.js.org">NextAuth.js</a> for
+            authentication.
+          </Typography>
+        </Grid>
+      </Grid>
     </Layout>
-  )
+  );
 }
