274daed7631166975080d579f5ff73c77e7be8a9
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-05-02 18:11:28 -0500

    finally... renamed file

--- components/NextLinkComposed.tsx
+++ components/NextLinkComposed.tsx
@@ -14,39 +14,38 @@ interface NextLinkComposedProps
   to: NextLinkProps["href"];
   linkAs?: NextLinkProps["as"];
 }
-const NextLinkComposed = forwardRef<
-  HTMLAnchorElement,
-  NextLinkComposedProps
->(function NextLinkComposed(props, ref) {
-  const {
-    to,
-    linkAs,
-    replace,
-    scroll,
-    shallow,
-    prefetch,
-    legacyBehavior = true,
-    locale,
-    ...other
-  } = props;
+const NextLinkComposed = forwardRef<HTMLAnchorElement, NextLinkComposedProps>(
+  function NextLinkComposed(props, ref) {
+    const {
+      to,
+      linkAs,
+      replace,
+      scroll,
+      shallow,
+      prefetch,
+      legacyBehavior = true,
+      locale,
+      ...other
+    } = props;
 
-  return (
-    <NextLink
-      href={to}
-      prefetch={prefetch}
-      as={linkAs}
-      replace={replace}
-      scroll={scroll}
-      shallow={shallow}
-      passHref
-      locale={locale}
-      legacyBehavior={legacyBehavior}>
-      <Anchor
-        ref={ref}
-        {...other}
-      />
-    </NextLink>
-  );
-});
+    return (
+      <NextLink
+        href={to}
+        prefetch={prefetch}
+        as={linkAs}
+        replace={replace}
+        scroll={scroll}
+        shallow={shallow}
+        passHref
+        locale={locale}
+        legacyBehavior={legacyBehavior}>
+        <Anchor
+          ref={ref}
+          {...other}
+        />
+      </NextLink>
+    );
+  }
+);
 
 export default NextLinkComposed;
